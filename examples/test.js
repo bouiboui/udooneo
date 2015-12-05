99963338d26c3c32f50629a664cad014951be2de
julien <julien@montpellier.salon>
2015-12-05 16:00:45 +0100

    Removes watch nonsense since it can't read values

--- examples/test.js
+++ examples/test.js
@@ -1,17 +1,8 @@
 var neo = require("../udooneo");
 
-neo.gpios.each(function (gpio) {
-    console.log("GPIO " + gpio.num() + " value = " + gpio.val());
-    gpio.watch(function () {
-        console.log("GPIO " + gpio.num() + " new value = " + gpio.val());
-    });
-});
-
 var gpio = new neo.GPIO(37).out();
 
 var x = 0;
 setInterval(function () {
-
     gpio.val(x++ % 2 ? neo.VALUE.LOW : neo.VALUE.HIGH);
-
 }, 1000);
